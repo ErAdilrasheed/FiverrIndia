ff32a6c4df8c72122fd3d5801f878800c2dee768
Mohammad Adil Rasheed <er.adilrasheed@gmail.com>
2024-05-25 14:58:25 +0530

    iub

--- server/middlewares/AuthMiddleware.js
+++ server/middlewares/AuthMiddleware.js
@@ -1,7 +1,8 @@
 import jwt from "jsonwebtoken";
 
 export const verifyToken = (req, res, next) => {
-  const token = JSON.parse(req.cookies.jwt).jwt;
+  const token = req.cookies.jwt;
+  const req = JSON.parse(token);
   if (!token) return res.status(401).send("You are not authenticated!");
   jwt.verify(token, process.env.JWT_KEY, async (err, payload) => {
     if (err) return res.status(403).send("Token is not valid!");
