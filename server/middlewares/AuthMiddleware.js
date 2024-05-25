5d960a56c5b77825c2c160e88fea752586613edb
Mohammad Adil Rasheed <er.adilrasheed@gmail.com>
2024-05-25 15:29:02 +0530

    hk

--- server/middlewares/AuthMiddleware.js
+++ server/middlewares/AuthMiddleware.js
@@ -4,8 +4,8 @@ export const verifyToken = (req, res, next) => {
   console.log(req.header);
 
   // const token = req.cookies.jwt;
-  const token = req.headers.authorization.split(" ")[1];
-  console.log("token12345", token);
+  const token = req.header.authorization.split(" ")[1];
+  console.log("token:= ", token);
   if (!token) return res.status(401).send("You are not authenticated!");
   jwt.verify(token, process.env.JWT_KEY, async (err, payload) => {
     if (err) return res.status(403).send("Token is not valid!");
