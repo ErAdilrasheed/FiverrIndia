42b5197c2f7f6c63556356f0220d18153ba73ac6
Mohammad Adil Rasheed <er.adilrasheed@gmail.com>
2024-05-25 15:17:49 +0530

    rthj

--- client/src/pages/profile.jsx
+++ client/src/pages/profile.jsx
@@ -10,6 +10,7 @@ import axios from "axios";
 import Image from "next/image";
 import { useRouter } from "next/router";
 import React, { useEffect, useState } from "react";
+import { useCookies } from "react-cookie";
 
 function Profile() {
   const router = useRouter();
